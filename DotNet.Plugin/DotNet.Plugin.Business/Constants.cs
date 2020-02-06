9a80de5d0d25aed95278c29b62db449d41ce7d2f
Ido Korkin <idokorkin1@gmail.com>
2020-02-06 10:00:59 +0200

    Update Constants.cs

--- DotNet.Plugin/DotNet.Plugin.Business/Constants.cs
+++ DotNet.Plugin/DotNet.Plugin.Business/Constants.cs
@@ -5,7 +5,7 @@ namespace DotNet.Plugin.Business
 	public static class Constants
 	{
 		//The folder name which contains the plugin DLLs
-		public const string FolderName = "Plugins";
+		public const string FolderName = "C:/Users/IDOKORKIN.WWEST/Downloads/HelloWorld";
 	}
 }
 
